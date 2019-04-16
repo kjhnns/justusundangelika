aea1bf57f24a405791de8e5ed3843b2222f7f812
Johannes Klumpe <johannes.klumpe@gmail.com>
2019-04-16 17:22:22 +0200

    Initial commit from gatsby: (https://github.com/gregoralbrecht/gatsby-starter-styled.git)

---
+++ src/util/style/index.js
@@ -0,0 +1,18 @@
+import * as styledComponents from 'styled-components'
+import { themeGet, px } from 'styled-system'
+import { theme } from './theme'
+
+// Utility function to get pixel values from a unitless scale in our custom theme
+const themePx = (...args) => props => px(themeGet(...args)(props))
+
+const {
+  default: styled,
+  css,
+  createGlobalStyle,
+  keyframes,
+  ThemeProvider,
+} = styledComponents
+
+export { css, createGlobalStyle, keyframes, ThemeProvider, theme, themePx }
+export * from 'styled-system'
+export default styled
